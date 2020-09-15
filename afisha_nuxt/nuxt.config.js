662e8f671a262a6ba41223d1d0541f5995e51701
Gaggsta <gratesklol@gmail.com>
2020-09-16 00:21:47 +0300

    yandex

--- afisha_nuxt/nuxt.config.js
+++ afisha_nuxt/nuxt.config.js
@@ -23,6 +23,10 @@ export default {
       charset: 'utf-8'
     },
     {
+      name: "yandex-verification",
+      content: "82f916d3b1843847"
+    },
+    {
       name: 'viewport',
       content: 'width=device-width, initial-scale=1'
     },
